c56b319544dfb18bb0dd01f4befeee106a3ad760
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-11-09 02:50:56 +0200

    Add a simple grammar and print parse trees for an input.

--- src/main/java/com/tbgdn/App.java
+++ src/main/java/com/tbgdn/App.java
@@ -1,13 +1,30 @@
 package com.tbgdn;
 
+
+import com.tbgdn.grammar.ArrayInitLexer;
+import com.tbgdn.grammar.ArrayInitParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.Trees;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        CharStream input = CharStreams.fromStream(System.in);
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        parser.init().value()
+                .stream()
+                .flatMap(val -> val.children.stream())
+                .map(Trees::toStringTree)
+                .forEach(System.out::print);
     }
 }
