c5a0d796aedd624357c8d1633a46e90e27d10096
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2019-11-12 22:17:05 +0200

    Print the tree and the result of walking it.

--- src/main/java/com/tbgdn/App.java
+++ src/main/java/com/tbgdn/App.java
@@ -3,28 +3,29 @@ package com.tbgdn;
 
 import com.tbgdn.grammar.ArrayInitLexer;
 import com.tbgdn.grammar.ArrayInitParser;
-import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.Trees;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        CharStream input = CharStreams.fromStream(System.in);
+public class App {
+    public static void main(String[] args) {
+        ArrayInitParser parser = createParser();
+        ArrayInitParser.InitContext context = parser.init();
+        System.out.println(context.toStringTree(parser));
+        //
+        ParseTreeWalker walker = new ParseTreeWalker();
+        context = createParser().init();
+        walker.walk(new ShortToUnicode(), context);
+    }
+
+    private static ArrayInitParser createParser() {
+        CharStream input = CharStreams.fromString("{1,2,3}");
         ArrayInitLexer lexer = new ArrayInitLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ArrayInitParser parser = new ArrayInitParser(tokens);
-        parser.init().value()
-                .stream()
-                .flatMap(val -> val.children.stream())
-                .map(Trees::toStringTree)
-                .forEach(System.out::print);
+        return new ArrayInitParser(tokens);
     }
 }
---
+++ src/main/java/com/tbgdn/ShortToUnicode.java
@@ -0,0 +1,23 @@
+package com.tbgdn;
+
+import com.tbgdn.grammar.ArrayInitBaseListener;
+import com.tbgdn.grammar.ArrayInitParser;
+
+public class ShortToUnicode extends ArrayInitBaseListener {
+
+    @Override
+    public void enterInit(ArrayInitParser.InitContext ctx) {
+        System.out.print("'");
+    }
+
+    @Override
+    public void exitInit(ArrayInitParser.InitContext ctx) {
+        System.out.println("'");
+    }
+
+    @Override
+    public void enterValue(ArrayInitParser.ValueContext ctx) {
+        int value = Integer.parseInt(ctx.getText());
+        System.out.printf("\\u%04x", value);
+    }
+}
